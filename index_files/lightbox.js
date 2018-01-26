90d7d01dcb0102889fe26f3df4b5fedd7b11afba
Kelly Monson <monson.exe@gmail.com>
2018-01-26 17:12:44 -0500

    initial commit

---
+++ index_files/lightbox.js
@@ -0,0 +1,1 @@
+(function(){var a=document,b=a.createElement('script');b.type='text/javascript';b.async=!0;b.src='//www.lightboxcdn.com/vendor/915a8e9b-430c-47ad-9809-4249fbeacffe/user'+(a.location.href.indexOf('boxqamode')>0?'_qa':'')+'.js?cb=636523454594293137';a=a.getElementsByTagName('script')[0];a.parentNode.insertBefore(b,a)})();
